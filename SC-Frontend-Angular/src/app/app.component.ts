79668920ab63d45780aad2dae3c3302b0ccc5c94
HarashilBurgos <1491109@senati.pe>
2024-11-14 21:32:39 -0500

    empezando graficos

--- SC-Frontend-Angular/src/app/app.component.ts
+++ SC-Frontend-Angular/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarraLateralComponent } from './MainComponents/barra-lateral/barra-lateral.component';
 import { EncabezadoComponent } from './MainComponents/encabezado/encabezado.component';
-
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BarraLateralComponent, EncabezadoComponent],
+  imports: [RouterOutlet, DashboardComponent, BarraLateralComponent, EncabezadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
